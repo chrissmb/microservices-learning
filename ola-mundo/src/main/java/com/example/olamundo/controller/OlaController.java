390d9d2886265d11b1ae89611af00aab022de657
chrissmb <chrissantista@gmail.com>
2022-04-13 23:28:04 +0000

    rota com path variable

--- ola-mundo/src/main/java/com/example/olamundo/controller/OlaController.java
+++ ola-mundo/src/main/java/com/example/olamundo/controller/OlaController.java
@@ -7,6 +7,7 @@ import com.example.olamundo.exception.NegocioException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,5 +30,13 @@ public class OlaController {
     @GetMapping("/minhas-props")
     public MinhasProps getMinhasProps() {
         return minhasProps;
+    }
+
+    @GetMapping("/sum/{value1}/{value2}")
+    public Integer sum(@PathVariable("value1") Integer value1, @PathVariable("value2") Integer value2) {
+        if (value1 == null || value2 == null) {
+            throw new NegocioException("Números não informados.");
+        }
+        return value1 + value2;
     }
 }
